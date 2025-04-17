1a311b241fac5a291dfba94ccd580930a1d4f8d3
AmanKrs <imkumarak4@gmail.com>
2025-04-18 04:29:58 +0530

    todoList v 1.1

--- 01_todo-localstorage/script.js
+++ 01_todo-localstorage/script.js
@@ -1,24 +1,56 @@
-const todoInput = document.getElementById("todo-input");
-const addTodoTask = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+//creating DOMContentLoaded event listener to verify the dom is loaded and then my function will start work
 
-let tasks = [];
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTodoTask = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
-addTodoTask.addEventListener("click", () => {
-  const todoTask = todoInput.value.trim();
-  if (todoTask === "") {
-    return;
-  } else {
+  let tasks = [];
+
+  if (localStorage.getItem("todoToken") !== null) {
+    const taskList = localStorage.getItem("todoToken");
+    tasks = JSON.parse(taskList);
+    // console.log(tasks);
+  }
+  console.log(tasks);
+
+  tasks.map((elem) => {
+    // console.log(elem);
+    renderTodoTask(elem);
+  });
+  addTodoTask.addEventListener("click", () => {
+    const todoTaskValue = todoInput.value.trim();
+
+    //checking if input is empty or not
+    if (todoTaskValue === "") {
+      return;
+    }
+    //creating object for todo task
+    const newTodoTask = {
+      id: Date.now(),
+      task: todoTaskValue,
+      isCompleted: false,
+    };
+
+    tasks.push(newTodoTask);
+    saveTask();
+    todoInput.value = ""; //clearing input value
+    renderTodoTask(newTodoTask);
+  });
+
+  function renderTodoTask(taskcreated) {
+    console.log(taskcreated);
     let newTodo = document.createElement("li");
-    newTodo.textContent = todoTask;
+    newTodo.textContent = taskcreated.task;
     let delbtn = document.createElement("button");
     delbtn.textContent = "Delete";
     newTodo.appendChild(delbtn);
     todoList.appendChild(newTodo);
-
-    delbtn.addEventListener("click", ()=>{
-      console.log(this)
-    })
-
   }
+
+  function saveTask() {
+    localStorage.setItem("todoToken", JSON.stringify(tasks));
+  }
+
+  
 });
